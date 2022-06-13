a779ee4951ffa30ad1456b2b43e5efe4ec37e7c5
Alfred Garcia <56953358+AlfredDev@users.noreply.github.com>
2022-06-13 11:24:46 -0500

    Delete Nutririon-Backend directory

--- Nutririon-Backend/src/main/java/com/example/nutririonbackend/Repository/DireccionPacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.nutririonbackend.Repository;
-
-import com.example.nutririonbackend.Model.DireccionPaciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DireccionPacienteRepository extends JpaRepository<DireccionPaciente,Integer> {
-}
--- Nutririon-Backend/src/main/java/com/example/nutririonbackend/Repository/PacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.nutririonbackend.Repository;
-
-import com.example.nutririonbackend.Model.Paciente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PacienteRepository extends JpaRepository<Paciente,Integer> {
-}
